a2ec36cd20fd00169f626a5308f40d790a865d4b
Kazuki Sawada <kazuki@6715.jp>
2017-07-13 07:24:32 +0900

    wait less

--- kiritan.py
+++ kiritan.py
@@ -13,9 +13,20 @@ from win32gui import *
 from win32process import *
 
 # 共通設定
-waitSec = 0.75
+waitSec = 0.1
 windowName = "VOICEROID＋ 東北きりたん EX"
 
+# 子ウィンドウの検索
+def enum_child_windows(window):
+	result = []
+	
+	def callback(hwnd, param):
+		result.append((hwnd, GetClassName(hwnd), GetWindowText(hwnd)))
+	
+	EnumChildWindows(window, callback, None)
+	return result
+
+# VOICELOIDを操作してWAV生成
 def talk(inputText):
 	logging.info("Generating WAV")
 	
@@ -33,22 +44,18 @@ def talk(inputText):
 
 	# 一時ファイルが存在している間は待つ
 	tmpfile = "tmp.wav"
-	while True:
-		if os.path.exists(outfile):
-			time.sleep(waitSec)
-		else:
-			break
+	while os.path.exists(outfile):
+		time.sleep(waitSec)
 	
 	while True:
 		# VOICEROIDプロセスを探す
 		window = FindWindow(None, windowName) or FindWindow(None, windowName + "*")
+		if window:
+			break
 		
 		# 見つからなかったらVOICEROIDを起動
-		if window == 0:
-			subprocess.Popen(["C:\Program Files (x86)\AHS\VOICEROID+\KiritanEX\VOICEROID.exe"])
-			time.sleep(3 * waitSec)
-		else:
-			break
+		subprocess.Popen(["C:\Program Files (x86)\AHS\VOICEROID+\KiritanEX\VOICEROID.exe"])
+		time.sleep(32 * waitSec)
 	
 	while True:
 		# ダイアログが出ていたら閉じる
@@ -62,62 +69,49 @@ def talk(inputText):
 	# 最前列に持ってくる
 	SetWindowPos(window, HWND_TOPMOST, 0, 0, 0, 0, SWP_SHOWWINDOW | SWP_NOMOVE | SWP_NOSIZE)
 	
-	# 保存ダイアログの操作
-	def enumDialogCallback(hwnd, param):
-		className = GetClassName(hwnd)
-		winText = GetWindowText(hwnd)
-		
-		# ファイル名を設定
-		if className.count("Edit"):
-			SendMessage(hwnd, WM_SETTEXT, 0, tmpfile)
+	# VOICEROID操作(保存ダイアログを出すまで)
+	def __req_speech():
+		for hwnd, className, windowText in enum_child_windows(window):
+			# テキストを入力する
+			if className.count("RichEdit20W"):
+				SendMessage(hwnd, WM_SETTEXT, 0, inputText)
 			
-		# 保存する
-		if winText.count("保存"):
-			SendMessage(hwnd, WM_LBUTTONDOWN, MK_LBUTTON, 0)
-			SendMessage(hwnd, WM_LBUTTONUP, 0, 0)
-		
-	# 音声の保存
-	def save():
-		time.sleep(waitSec)
-		
-		# ダイアログがあれば操作する
+			if windowText.count("音声保存"):
+				# 最小化解除
+				ShowWindow(window, SW_SHOWNORMAL)
+				
+				# 保存ボタンを押す
+				SendMessage(hwnd, WM_LBUTTONDOWN, MK_LBUTTON, 0)
+				SendMessage(hwnd, WM_LBUTTONUP, 0, 0)
+	
+	# 別スレッドで実行(保存ダイアログを出すとSendMessageがブロックする)
+	threading.Thread(target=__req_speech).start()
+	
+	# 保存ダイアログを探す
+	while True:
 		dialog = FindWindow(None, "音声ファイルの保存")
 		if dialog:
-			EnumChildWindows(dialog, enumDialogCallback, None)
-			return
+			break
 		
-		# 再試行
-		save()
-		
-	# VOICEROIDを操作
-	def enumCallback(hwnd, param):
-		className = GetClassName(hwnd)
-		winText = GetWindowText(hwnd)
-		
-		# テキストを入力する
-		if className.count("RichEdit20W"):
-			SendMessage(hwnd, WM_SETTEXT, 0, inputText)
-		
-		if winText.count("音声保存"):
-			# 最小化解除
-			ShowWindow(window, SW_SHOWNORMAL)
-			
-			# 保存ダイアログ操作用スレッド起動
-			threading.Thread(target=save).start()
+		time.sleep(waitSec)
+	
+	# 保存ボタンを押す
+	while FindWindow(None, "音声ファイルの保存"):
+		for hwnd, className, windowText in enum_child_windows(dialog):
+			# ファイル名を入力
+			if className.count("Edit"):
+				SendMessage(hwnd, WM_SETTEXT, 0, tmpfile)
 			
 			# 保存ボタンを押す
-			SendMessage(hwnd, WM_LBUTTONDOWN, MK_LBUTTON, 0)
-			SendMessage(hwnd, WM_LBUTTONUP, 0, 0)
+			if windowText.count("保存"):
+				SendMessage(hwnd, WM_LBUTTONDOWN, MK_LBUTTON, 0)
+				SendMessage(hwnd, WM_LBUTTONUP, 0, 0)
 		
-	# VOICEROIDにテキストを読ませる
-	EnumChildWindows(window, enumCallback, None)
-
+		time.sleep(waitSec)
+	
 	# プログレスダイアログが表示されている間は待つ
-	while True:
-		if FindWindow(None, "音声保存"):
-			time.sleep(waitSec)
-		else:
-			break
+	while FindWindow(None, "音声保存"):
+		time.sleep(waitSec)
 	
 	try:
 		# ファイルを移動
